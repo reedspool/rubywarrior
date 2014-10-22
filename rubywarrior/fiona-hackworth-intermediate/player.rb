8183529a6d596b1ff4f306ef3f3cc29954db9da1
reedspool <splinterspool@yahoo.com>
2014-10-22 00:03:32 -0400

    Level 2 was hard. Read the clue. meow.

--- rubywarrior/fiona-hackworth-intermediate/player.rb
+++ rubywarrior/fiona-hackworth-intermediate/player.rb
@@ -1,5 +1,25 @@
 class Player
+
+	@@max_health = 20
+	@@dirs = [ :forward, :left, :backward, :right  ]
+
   def play_turn(warrior)
-    warrior.walk! warrior.direction_of_stairs
+
+  	stairs = warrior.direction_of_stairs
+
+  	danger = @@dirs.reduce(false) do |memo, dir|
+  		
+  		bad = warrior.feel(dir).enemy? && dir
+
+  		bad || memo
+  	end
+
+  	should_really_rest = warrior.health < @@max_health
+
+  	warrior.attack!(danger) && return if danger
+
+  	warrior.rest! && return if should_really_rest
+
+  	warrior.walk! stairs
   end
 end
